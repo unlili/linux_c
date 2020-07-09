d2f7f7f6ad644b634ef6f4c16dc7cb6c4647a685
Zihang <45307866+unlili@users.noreply.github.com>
2020-07-09 21:00:44 +0800

    chmod修改文件权限

--- C/sysIO/chmod.c
+++ C/sysIO/chmod.c
@@ -9,8 +9,8 @@ int main(int argc,char * argv[])
         printf("missing parameter");
         exit(1);
     }
-    
-    long int a = strtol(argv[2],NULL,8);
+    //  把一个字符串变成8进制整型   '777' --> int 777
+    long int a = strtol(argv[2],NULL,8);//修改参数argv[1]的权限修改为agrv[2]
     
     int ret = chmod(argv[1],a);
     if(ret == -1)
