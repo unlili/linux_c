ace014799be13a6833dbae5bb486e9aca28eaa79
Zihang <45307866+unlili@users.noreply.github.com>
2020-07-21 22:37:01 +0800

    execlp函数

---
+++ C/sys/process/execlp_lp.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+
+int main(void)
+{
+    pid_t pid = fork();
+
+
+
+    if(pid == -1) 
+    {   
+        perror("fork fail");
+        exit(1);
+    }   
+    else if(pid > 0)//父进程返回子进程的pid
+    {   
+        sleep(2);   
+    }   
+    else if(pid == 0)
+    {   
+        execlp("ls","ls","-l","-a","-h",NULL);
+    }
+
+    return 0;
+}
